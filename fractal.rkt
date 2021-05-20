3bb5ad9e9ca60a42651aeae4c089eff7c0ca7d01
Caio Homar <caioah@users.noreply.github.com>
2021-05-20 09:02:11 -0300

    Add files via upload

--- fractal.rkt
+++ fractal.rkt
@@ -1,142 +1,150 @@
 #lang racket
-(require 2htdp/universe racket/draw racket/generator images/logos)
+(require 2htdp/universe racket/draw racket/generator images/logos racket/flonum)
 ;----------- const --------------------
 (define log2 (log 2))
 (define (sqr/sc x) (/ (sqr x) (scale)))
 ;----------- independent parameters -----------
 (define zoom (make-parameter 1/2))
 (define limit (make-parameter 100))
-(define bailout (make-parameter 100))
+(define bailout (make-parameter 100.0))
 (define center (make-parameter 0))
 ;-------------------- others --------------------------
 (define radius (make-derived-parameter bailout sqr sqrt))
 (define view (make-parameter 600+450i))
-(define scale (make-parameter 225))
+(define scale (make-parameter 225.0))
 (define (set-size! x)
   (scale (* 1/2 (min (real-part x) (imag-part x))))
   (view x))
 
-(define sac-stripe (make-parameter 5))
+(define sac-stripe (make-parameter 5.0))
 (define sac-trunc (make-parameter 1))
 ;----------------------- fractal types ---------------------------
 (define (mandelbrot-gen c)
-  (let ([i (/ 2 (scale))])
+  (let ([i (fl/ 2.0 (scale))])
     (generator ()
-               (let loop ([z (exact->inexact c)] [x2 (exact->inexact (sqr/sc (real-part c)))]
-                          [y2 (exact->inexact (sqr/sc (imag-part c)))])
+               (let loop ([z c] [x2 (fl/ (flexpt (flreal-part c) 2.0) (scale))]
+                          [y2 (fl/ (flexpt (flimag-part c) 2.0) (scale))])
                (yield z x2 y2)
-               (let ([t (+ c (make-rectangular (- x2 y2) (* i (real-part z) (imag-part z))))])
-                 (loop t (sqr/sc (real-part t)) (sqr/sc (imag-part t))))))))
+               (let ([t (make-flrectangular (fl+ (flreal-part c) (fl- x2 y2))
+                                            (fl+ (flimag-part c) (fl* i (flreal-part z) (flimag-part z))))])
+                 (loop t (fl/ (flexpt (flreal-part t) 2.0) (scale))
+                       (fl/ (flexpt (flimag-part t) 2.0) (scale))))))))
 (define (bship-gen c)
-  (let ([i (/ 2 (scale))])
+  (let ([i (fl/ 2.0 (scale))])
     (generator ()
-               (let loop ([z (exact->inexact c)] [x2 (exact->inexact (sqr/sc (real-part c)))]
-                          [y2 (exact->inexact (sqr/sc (imag-part c)))])
+               (let loop ([z c] [x2 (fl/ (flexpt (flreal-part c) 2.0) (scale))]
+                          [y2 (fl/ (flexpt (flimag-part c) 2.0) (scale))])
                (yield z x2 y2)
-               (let ([t (+ c (make-rectangular (- x2 y2) (* -1 (abs (* i (real-part z) (imag-part z))))))])
-                 (loop t (sqr/sc (real-part t)) (sqr/sc (imag-part t))))))))
+               (let ([t (make-flrectangular (fl+ (flreal-part c) (fl- x2 y2))
+                                            (fl- (flimag-part c) (flabs (fl* i (flreal-part z) (flimag-part z)))))])
+                 (loop t (fl/ (flexpt (flreal-part t) 2.0) (scale))
+                       (fl/ (flexpt (flimag-part t) 2.0) (scale))))))))
 (define (julia-gen p)
-  (let ([j (exact->inexact (* p (/ (scale) (zoom))))] [i (/ 2 (scale))])
+  (let ([j (exact->inexact (* p (scale)))] [i (fl/ 2.0 (scale))])
   (λ (c) (generator ()
-                    (let loop ([z (exact->inexact c)] [x2 (exact->inexact (sqr/sc (real-part c)))]
-                               [y2 (exact->inexact (sqr/sc (imag-part c)))])
-                      (yield z x2 y2)
-                      (let ([t (+ j (make-rectangular (- x2 y2) (* i (real-part z) (imag-part z))))])
-                        (loop t (sqr/sc (real-part t)) (sqr/sc (imag-part t)))))))))
+                    (let loop ([z c] [x2 (fl/ (flexpt (flreal-part c) 2.0) (scale))]
+                          [y2 (fl/ (flexpt (flimag-part c) 2.0) (scale))])
+               (yield z x2 y2)
+               (let ([t (make-flrectangular (fl+ (flreal-part j) (fl- x2 y2))
+                                            (fl+ (flimag-part j) (fl* i (flreal-part z) (flimag-part z))))])
+                 (loop t (fl/ (flexpt (flreal-part t) 2.0) (scale))
+                       (fl/ (flexpt (flimag-part t) 2.0) (scale)))))))))
 (define (bship-test c)
-  (or (and (zero? (imag-part c))
-           (<= (real-part c) (/ (scale) 4))
-           (>= (real-part c) (* -2 (scale))))
-      (and (positive? (imag-part c))
-           (<= (real-part c) (/ (scale) 4))
-           (let ([ys (sqr (imag-part c))])
-             (or (< (/ (+ (sqr (+ (real-part c) (scale))) ys) (scale)) (/ (scale) 16))
-                 (let* ([t (- (real-part c) (/ (scale) 4))]
-                        [q (/ (+ (sqr t) ys) (scale))])
-                   (< (* q (+ q t)) (/ ys 4))))))))
+  (or (and (zero? (flimag-part c))
+       (fl<= (flreal-part c) (fl/ (scale) 4.0))
+       (fl>= (flreal-part c) (fl* -2.0 (scale))))
+      (and (positive? (flimag-part c))
+           (fl<= (flreal-part c) (fl/ (scale) 4.0))
+           (let ([ys (flexpt (flimag-part c) 2.0)])
+             (or (fl< (fl/ (fl+ (flexpt (fl+ (flreal-part c) (scale)) 2.0) ys) (scale)) (fl/ (scale) 16.0))
+                 (let* ([t (fl- (flreal-part c) (fl/ (scale) 4.0))]
+                        [q (fl/ (fl+ (flexpt t 2.0) ys) (scale))])
+                   (fl< (fl* q (fl+ q t)) (fl/ ys 4.0))))))))
 (define (mandelbrot-test c)
-  (or (and (zero? (imag-part c))
-       (<= (real-part c) (/ (scale) 4))
-       (>= (real-part c) (* -2 (scale))))
-      (let ([ys (sqr (imag-part c))])
-        (or (< (/ (+ (sqr (+ (real-part c) (scale))) ys) (scale)) (/ (scale) 16))
-            (let* ([t (- (real-part c) (/ (scale) 4))]
-                   [q (/ (+ (sqr t) ys) (scale))])
-              (< (* q (+ q t)) (/ ys 4)))))))
+  (or (and (zero? (flimag-part c))
+       (fl<= (flreal-part c) (fl/ (scale) 4.0))
+       (fl>= (flreal-part c) (fl* -2.0 (scale))))
+      (let ([ys (flexpt (flimag-part c) 2.0)])
+        (or (fl< (fl/ (fl+ (flexpt (fl+ (flreal-part c) (scale)) 2.0) ys) (scale)) (fl/ (scale) 16.0))
+            (let* ([t (fl- (flreal-part c) (fl/ (scale) 4.0))]
+                   [q (fl/ (fl+ (flexpt t 2.0) ys) (scale))])
+              (fl< (fl* q (fl+ q t)) (fl/ ys 4.0)))))))
 (define-syntax (mandelbrot-escape stx)
   (syntax-case stx (dem)
-    [(_ dem) #'(dem mandelbrot-gen mandelbrot-test 1 1 1)]
+    [(_ dem) #'(dem mandelbrot-gen mandelbrot-test 1 1.0 1.0)]
     [(_ esc args ...) #'(esc mandelbrot-gen mandelbrot-test 1 args ...)]))
 (define-syntax (julia-escape stx)
   (syntax-case stx (dem)
-    [(_ p dem) #'(dem (julia-gen p) (λ (x) #f) 0 1 0)]
+    [(_ p dem) #'(dem (julia-gen p) (λ (x) #f) 0 1.0 0.0)]
     [(_ p esc args ...) #'(esc (julia-gen p) (λ (x) #f) 0 args ...)]))
 (define-syntax (bship-escape stx)
   (syntax-case stx (dem)
-    [(_ dem) #'(dem bship-gen bship-test 1 1 1)]
+    [(_ dem) #'(dem bship-gen bship-test 1 1.0 1.0)]
     [(_ esc args ...) #'(esc bship-gen bship-test 1 args ...)]))
 
 ;--------------------- escapes ----------------------------------
 (define (normal gen test start)
   (λ (c) (if (test c) 0 (- 1 (/ (point-loop (n start) (gen z x2 y2) #f c n) (limit))))))
 (define (potential gen test start)
-  (let ([ls (log (scale))] [b (* (scale) (bailout))])
-    (λ (c) (if (test c) 0 ((λ (p) (if (<= (cdr p) b) 0
-                                      (expt (* 4 (/ (- (log (cdr p)) ls)
-                                                    (arithmetic-shift 1 (add1 (car p)))))
-                                            (/ 1 (+ 7 (integer-length (exact-floor (zoom))))))))
-                           (point-loop (n start) (gen z x2 y2) #f c (cons n (+ x2 y2))))))))
+  (let ([ls (log (scale))] [b (fl* (scale) (bailout))])
+    (λ (c) (if (test c) 0.0
+               ((λ (p) (if (fl<= (cdr p) b) 0.0
+                           (flexpt (fl* 4.0 (fl/ (fl- (fllog (cdr p)) ls)
+                                                 (->fl (arithmetic-shift 1 (add1 (car p))))))
+                                 (fl/ 1.0 (->fl (+ 7 (integer-length (exact-floor (zoom)))))))))
+                (point-loop (n start) (gen z x2 y2) #f c (cons n (fl+ x2 y2))))))))
 (define (smooth-par b b2 lb ls)
-  (λ (mag) (cond [(<= mag b) 1]
-                 [(>= mag b2) 0]
-                 [else (add1 (- lb (/ (log (- (log mag) ls)) log2)))])))
+  (λ (mag) (cond [(fl<= mag b) 1.0]
+                 [(fl>= mag b2) 0.0]
+                 [else (fl+ 1.0 (fl- lb (fl/ (fllog (- (fllog mag) ls)) log2)))])))
 (define (smooth gen test start)
-  (let ([par (smooth-par (* (scale) (bailout)) (* (scale) (sqr (bailout))) (/ (log (log (bailout))) log2) (log (scale)))])
+  (let ([par (smooth-par (* (scale) (bailout)) (* (scale) (flexpt (bailout) 2.0))
+                         (fl/ (fllog (fllog (bailout))) log2) (fllog (scale)))])
     (λ (c) (if (test c) 0
-               ((λ (p) (- 1 (/ (+ (car p) (par (cdr p))) (add1 (limit)))))
+               ((λ (p) (fl- 1.0 (/ (fl+ (->fl (car p)) (par (cdr p))) (->fl (add1 (limit))))))
                 (point-loop (n start) (gen z x2 y2) #f c (cons n (+ x2 y2))))))))
 (define (dem-out ls b)
   (λ (d mag)
-    (if (or (zero? d) (<= mag b)) 0
-        (expt (* 4 (- (log mag) ls)
-                 (sqrt (/ mag (* (scale) (+ (sqr (real-part d)) (sqr (imag-part d)))))))
-              (/ 1 (+ 3 (integer-length (exact-floor (zoom)))))))))
+    (if (or (zero? d) (fl<= mag b)) 0
+        (flexpt (fl* 4.0 (fl- (log mag) ls)
+                 (sqrt (fl/ mag (fl* (scale) (fl+ (flexpt (real-part d) 2.0) (flexpt (imag-part d) 2.0))))))
+              (fl/ 1.0 (->fl (+ 3 (integer-length (exact-floor (zoom))))))))))
 (define (dem gen test start [init-d 1] [add 1])
-  (let ([i (/ 2 (scale))] [out (dem-out (log (scale)) (* (scale) (bailout)))])
+  (let ([i (fl/ 2.0 (scale))] [out (dem-out (log (scale)) (fl* (scale) (bailout)))])
     (λ (c) (let ([d init-d] [next-d init-d])
-             (if (test c) 0
+             (if (test c) 0.0
                  ((λ (mag) (out d mag))
                   (point-loop (n start) (gen z x2 y2) #f c
                               (set! d next-d) (set! next-d (+ add (* i d z))) (+ x2 y2))))))))
 (define (sac-t z)
-  (* 1/2 (add1 (sin (* (sac-stripe) (angle z))))))
+  (fl* 0.5 (fl+ 1.0 (sin (fl* (sac-stripe) (angle z))))))
 (define ((sac-out par) a-1 n z mag)
   (if (or (>= n (limit)) (< n (sac-trunc))) 0
       (let ([d (par mag)])
-        (+ (* d (/ (+ a-1 (sac-t z)) (- n -1 (sac-trunc))))
-           (if (> n (sac-trunc)) (* (- 1 d) (/ a-1 (- n (sac-trunc)))) 0)))))
+        (fl+ (fl* d (fl/ (fl+ a-1 (sac-t z)) (->fl (- n -1 (sac-trunc)))))
+           (if (> n (sac-trunc)) (fl* (fl- 1.0 d) (fl/ a-1 (->fl (- n (sac-trunc))))) 0.0)))))
 (define (sac gen test start)
-  (let* ([b (* (scale) (bailout))]
-         [out (sac-out (smooth-par b (* (scale) (sqr (bailout)))
-                                   (/ (log (log (bailout))) log2) (log (scale))))])
-    (λ (c) (let ([a-1 0])
-             (if (test c) 0
+  (let* ([b (fl* (scale) (bailout))]
+         [out (sac-out (smooth-par b (fl* (scale) (flexpt (bailout) 2.0))
+                                   (fl/ (fllog (fllog (bailout))) log2) (fllog (scale))))])
+    (λ (c) (let ([a-1 0.0])
+             (if (test c) 0.0
                  ((λ (lst) (apply out a-1 lst))
                   (point-loop (n start) (gen z x2 y2) #f c
-                              (let ([mag (+ x2 y2)])
-                                (unless (or (< n (sac-trunc)) (> mag b))
-                                  (set! a-1 (+ a-1 (sac-t z))))
+                              (let ([mag (fl+ x2 y2)])
+                                (unless (or (< n (sac-trunc)) (fl> mag b))
+                                  (set! a-1 (fl+ a-1 (sac-t z))))
                                 (list n z mag)))))))))
 (define ((point-trap up) gen test start)
-  (let ([p (* (/ (scale) (zoom)) up)])
+  (let ([p (* (scale) up)])
     (λ (c) (let ([trap +inf.0] [x +inf.0] [t +inf.0])
              (point-loop (n start) (gen z x2 y2) #f c
                          (set! t (- z p))
-                         (set! x (+ (sqr/sc (real-part t)) (sqr/sc (imag-part t))))
+                         (set! x (/ (+ (sqr (real-part t)) (sqr (imag-part t))) (scale)))
                          (when (< x trap) (set! trap x)))
              (sqrt (/ trap (scale)))))))
 (define ((line-trap up) gen test start)
-  (let ([p (* (/ (scale) (zoom)) up)])
+  (let ([p (* (scale) up)])
     (λ (c) (let ([trap +inf.0] [x +inf.0])
              (point-loop (n start) (gen z x2 y2) #f c
                          (set! x (min (abs (- (real-part z) (real-part p))) (abs (- (imag-part z) (imag-part p)))))
@@ -149,7 +157,7 @@
                                   (< (/ (real-part z) (real-part sz)) 1)
                                   (< (/ (imag-part z) (imag-part sz)) 1))
                          (set! trap z)))
-           (or trap 0))))
+           (or trap 0.0))))
 
 ;-------------------- colorizations --------------------------------
 (define (grayscale v)
@@ -183,7 +191,7 @@
     [(_ (n start) (gen z x2 y2) break c body ...)
      #'(for/last ([n (in-range start (add1 (limit)))]
                   [(z x2 y2) (in-producer (gen c))]
-                  #:final (> (/ (+ x2 y2) (scale)) (bailout)))
+                  #:final (fl> (fl/ (fl+ x2 y2) (scale)) (bailout)))
          #:break break
          body ...)]))
 (define (center-range sz p0 [step 1])
@@ -198,7 +206,7 @@
                    [b (in-naturals)] #:when #t
                    [x (center-range (real-part t) (real-part c) 1)]
                    [a (in-naturals)])
-               (let ([p (make-rectangular a b)] [val (call (make-rectangular x y))])
+               (let ([p (make-rectangular a b)] [val (call (make-flrectangular (->fl x) (->fl y)))])
                  body ...)))))]))
 (define-syntax-rule (fractal-bmp esc pix)
   (let ([bmp (make-object bitmap% (real-part (view)) (imag-part (view)))] [to-pixel pix])
@@ -209,22 +217,22 @@
 (define escape (make-parameter normal))
 (define type (make-parameter 'mandelbrot))
 (define coloring (make-parameter grayscale))
-(define modifier (make-parameter values))
 (define julia-param (make-parameter -0.8+0.156i))
 (define offset (make-parameter (/ (view) 2)))
-(define locked (make-parameter #t))
+(define text-color (make-parameter (make-object color% 0 0 0)))
+
 (define-syntax-rule (fractal-bmp-thread bmp)
   (thread (λ () (fractal-loop pos z (v (case (type)
                                          [(mandelbrot) (mandelbrot-escape (escape))]
                                          [(julia) (julia-escape (julia-param) (escape))]
                                          [(bship) (bship-escape (escape))]))
-                              (send bmp set-argb-pixels (real-part pos) (imag-part pos) 1 1 ((coloring) ((modifier) v)))))))
+                              (send bmp set-argb-pixels (real-part pos) (imag-part pos) 1 1 ((coloring) v))))))
 (define (draw-info bmp)
   (let ([dc (new bitmap-dc% [bitmap (make-object bitmap% (send bmp get-width) (send bmp get-height))])])
     (send dc draw-bitmap bmp 0 0)
     (send dc draw-line 0 (imag-part (offset)) (real-part (view)) (imag-part (offset)))
     (send dc draw-line (real-part (offset)) 0 (real-part (offset)) (imag-part (view)))
-    (send dc set-text-foreground (make-object color% 255 255 255))
+    (send dc set-text-foreground (text-color))
     (send dc draw-text (format "Pos: ~a" (exact->inexact (+ (center) (/ (- (conjugate (offset)) (conjugate (/ (view) 2)))
                                                                         (* (scale) (zoom)))))) 0 0)
     (send dc draw-text (format "Center: ~a" (exact->inexact (center))) 0 20)
@@ -244,7 +252,16 @@
                                  (zoom (* 2 (zoom)))
                                  (new-world w #t)]
                               [else (offset (make-rectangular x y)) w]))]
-  [on-key (λ (w k) (cond [(key=? k "add") (zoom (* 2 (zoom))) (new-world w)]
+  [on-key (λ (w k) (cond [(key=? k "\t")
+                          (text-color (make-object color% (- 255 (send (text-color) red))
+                                        (- 255 (send (text-color) green))
+                                        (- 255 (send (text-color) blue)))) w]
+                         [(key=? k "\r") (new-world w #t)]
+                         [(key=? k "left") (center (- (center) (/ 1/4 (zoom)))) (new-world w)]
+                         [(key=? k "right") (center (+ (center) (/ 1/4 (zoom)))) (new-world w)]
+                         [(key=? k "up") (center (+ (center) (/ +1/4i (zoom)))) (new-world w)]
+                         [(key=? k "down") (center (- (center) (/ +1/4i (zoom)))) (new-world w)]
+                         [(key=? k "add") (zoom (* 2 (zoom))) (new-world w)]
                          [(key=? k "subtract") (zoom (/ (zoom) 2)) (new-world w)]
                          [(key=? k "prior") (limit (+ (limit) 25)) (new-world w)]
                          [(key=? k "next") (limit (max 10 (- (limit) 25))) (new-world w)]
@@ -255,17 +272,20 @@
                          [(key=? k "2") (coloring (cos-pixel '(0 2/3 1))) (new-world w)]
                          [(key=? k "3") (coloring (hue '(0 0 0))) (new-world w)]
                          [(key=? k "4") (coloring (sin-pixel '(0 2/3 1))) (new-world w)]
-                         [(key=? k "f1") (escape normal) (modifier values) (new-world w)]
-                         [(key=? k "f2") (escape smooth) (modifier values) (new-world w)]
-                         [(key=? k "f3") (escape potential) (modifier values) (new-world w)]
-                         [(key=? k "f4") (escape dem) (modifier values) (new-world w)]
-                         [(key=? k "f5") (escape sac) (modifier values) (new-world w)]
-                         [(key=? k "f6") (escape (point-trap 0)) (modifier values) (new-world w)]
-                         [(key=? k "f7") (escape (line-trap 0)) (modifier values) (new-world w)]
-                         [(key=? k "f8") (escape (rect-trap (make-rectangular (send rect-bmp get-width) (send rect-bmp get-height))))
-                                         (modifier (λ (z) (let ([b (make-bytes 4 0)])
-                                                            (send rect-bmp get-argb-pixels (exact-round (real-part z))
-                                                                  (- (send rect-bmp get-height) (exact-round (imag-part z))) 1 1 b)
-                                                            b)))
-                                         (coloring values) (new-world w)]
+                         [(key=? k "q") (type 'mandelbrot) (new-world w)]
+                         [(key=? k "w") (type 'julia) (new-world w)]
+                         [(key=? k "e") (type 'bship) (new-world w)]
+                         [(key=? k "f1") (escape normal) (new-world w)]
+                         [(key=? k "f2") (escape smooth) (new-world w)]
+                         [(key=? k "f3") (escape potential) (new-world w)]
+                         [(key=? k "f4") (escape dem) (new-world w)]
+                         [(key=? k "f5") (escape sac) (new-world w)]
+                         [(key=? k "f6") (escape (point-trap 0)) (new-world w)]
+                         [(key=? k "f7") (escape (line-trap 0)) (new-world w)]
+                         [(key=? k "f8")
+                          (escape (rect-trap (make-rectangular (send rect-bmp get-width) (send rect-bmp get-height))))
+                          (coloring (λ (z) (let ([b (make-bytes 4 0)])
+                                             (send rect-bmp get-argb-pixels (exact-round (real-part z))
+                                                   (- (send rect-bmp get-height) (exact-round (imag-part z))) 1 1 b)
+                                             b))) (new-world w)]
                          [else w]))])
